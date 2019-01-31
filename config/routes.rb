079a5b72e5397ccb21a60c3458ccc245c1c091fa
evertsalinas <evertsalinas88@gmail.com>
2019-01-31 11:48:54 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :products, only: [:show]
 end
